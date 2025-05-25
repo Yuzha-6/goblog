a146f8a0eda803920638463a6b47a5d4fd323991
Yuzha-6 <1737594382@qq.com>
2025-05-25 18:07:26 +0800

    build --first day

---
+++ server/initialize/cron.go
@@ -0,0 +1,37 @@
+package initialize
+
+import (
+	"server/global"
+	"server/task"
+
+	"os"
+
+	"github.com/robfig/cron/v3"
+	"go.uber.org/zap"
+)
+
+type ZapLogger struct {
+	logger *zap.Logger
+}
+
+func (l *ZapLogger) Info(msg string, keysAndValues ...interface{}) {
+	l.logger.Info(msg, zap.Any("keysAndValues", keysAndValues))
+}
+
+func (l *ZapLogger) Error(err error, msg string, keysAndValues ...interface{}) {
+	l.logger.Error(msg, zap.Error(err), zap.Any("keysAndValues", keysAndValues))
+}
+
+func NewZaplogger() *ZapLogger {
+	return &ZapLogger{logger: global.Log}
+}
+
+func InitCron() {
+	c := cron.New(cron.WithLogger(NewZaplogger()))
+	err := task.RegisterScheduledTasks(c)
+
+	if err != nil {
+		global.Log.Error("Error scheduling cron job:", zap.Error(err))
+		os.Exit(1)
+	}
+}
---
+++ server/initialize/gorm.go
@@ -0,0 +1,30 @@
+package initialize
+
+import (
+	"server/global"
+
+	"os"
+
+	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func InitGorm() *gorm.DB {
+	mysqlCfg := global.Config.Mysql
+
+	db, err := gorm.Open(mysql.Open(mysqlCfg.Dsn()), &gorm.Config{
+		Logger: logger.Default.LogMode(mysqlCfg.LogLevel()),
+	})
+	if err != nil {
+		global.Log.Error("Failed to connect to MySQL:", zap.Error(err))
+		os.Exit(1)
+	}
+
+	sqlDB, _ := db.DB()
+	sqlDB.SetMaxIdleConns(mysqlCfg.MaxIdleConns)
+	sqlDB.SetMaxOpenConns(mysqlCfg.MaxOpenConns)
+
+	return db
+}
