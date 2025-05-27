d60cbe03f5da1ff1fbc0cd1c92263352044a9359
Yuzha-6 <1737594382@qq.com>
2025-05-27 09:10:55 +0800

    build sql_export_inport

--- server/flag/enter.go
+++ server/flag/enter.go
@@ -1,6 +1,7 @@
 package flag
 
 import (
+	"fmt"
 	"os"
 	"server/global"
 
@@ -13,9 +14,18 @@ var (
 		Name:  "sql",
 		Usage: "Initializes the structure of the MySQL database table.",
 	}
+	sqlExportFlag = &cli.BoolFlag{
+		Name:  "sql-export",
+		Usage: "Exports SQL data to a specified file.",
+	}
 )
 
 func Run(c *cli.Context) {
+	if c.NumFlags() > 1 {
+		err := cli.NewExitError("Only one command can be specified", 1)
+		global.Log.Error("Invalid command usage:", zap.Error(err))
+		os.Exit(1)
+	}
 	switch {
 	case c.Bool(sqlFlag.Name):
 		if err := SQL(); err != nil {
@@ -24,6 +34,15 @@ func Run(c *cli.Context) {
 		} else {
 			global.Log.Info("Successfully created table structure")
 		}
+	case c.Bool(sqlExportFlag.Name):
+		if err := SQLExport(); err != nil {
+			global.Log.Error("Failed to export SQL data:", zap.Error(err))
+		} else {
+			global.Log.Info("Successfully exported SQL data")
+		}
+	default:
+		err := cli.NewExitError("unknown command", 1)
+		global.Log.Error(err.Error(), zap.Error(err))
 	}
 }
 
@@ -32,17 +51,23 @@ func NewApp() *cli.App {
 	app.Name = "Go Blog"
 	app.Flags = []cli.Flag{
 		sqlFlag,
+		sqlExportFlag,
 	}
 	app.Action = Run
 	return app
 }
 
 func InitFlag() {
-	app := NewApp()
-	err := app.Run(os.Args)
-	if err != nil {
-		global.Log.Error("Application execution encountered an error:", zap.Error(err))
-		os.Exit(1)
+	if len(os.Args) > 1 {
+		app := NewApp()
+		err := app.Run(os.Args)
+		if err != nil {
+			global.Log.Error("Application execution encountered an error:", zap.Error(err))
+			os.Exit(1)
+		}
+		if os.Args[1] == "-h" || os.Args[1] == "-help" {
+			fmt.Println("Display help message...")
+		}
+		os.Exit(0)
 	}
-	os.Exit(0)
 }
